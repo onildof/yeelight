10553fba18e5b0d5e88d9dc9770ea709d502efaa
unknown <onildo@gmail.com>
2020-01-07 00:12:52 -0300

    Roubado. Agora exibe homepage view

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const port = 8282
+var ejs = require('ejs');
+
+// handle ejs pages
+app.engine(".html", ejs.__express);
+app.set("view engine", "html");
 
 app.use('/public', express.static(path.join(__dirname,'public')))
+app.use('/images', express.static(path.join(__dirname,'public', 'images')))
 
 app.get('/', function (req, res) {
-	res.send(`oi`)
+	res.render('index.html')
 })
 
 app.listen(port, () => console.log("Servidor HTTP escutando na porta "+port))

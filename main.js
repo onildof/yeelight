4a2e96a86f1237459fc8f2a087082cad314d8ceb
unknown <onildo@gmail.com>
2020-01-07 00:00:04 -0300

    Respondendo a uma requisição HTTP GET na raiz

--- main.js
+++ main.js
@@ -5,4 +5,8 @@ const port = 8282
 
 app.use('/public', express.static(path.join(__dirname,'public')))
 
+app.get('/', function (req, res) {
+	res.send(`oi`)
+})
+
 app.listen(port, () => console.log("Servidor HTTP escutando na porta "+port))
